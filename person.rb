281abb14a34b81b2adf9e6f742584f3e321b6baa
keza681 <lindakeza19@gmail.com>
2022-06-18 16:57:20 +0200

    Alter the person

--- person.rb
+++ person.rb
@@ -1,8 +1,12 @@
-class Person
+Require './nameable'
+
+class Person < Nameable
+
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -11,10 +15,12 @@ class Person
 
   def can_use_services?
     return true if of_age? || @parent_permission
-
-    false
   end
 
+  def correct_name
+    @name
+  end
+  
   private
 
   def of_age?
