c04057539ef27ddd7a1ac8d4fae7a99149ad0eb5
keza681 <lindakeza19@gmail.com>
2022-06-18 17:38:23 +0200

    Add rental and classroom to the project

---
+++ rental.rb
@@ -0,0 +1,11 @@
+class Rental
+    def initialize(data, book, person)
+      @data = data
+      @person = person
+      @book = book
+      person.rentals << self
+      book.rentals << self
+    end
+  
+    attr_accessor :date, :book, :person
+  end
