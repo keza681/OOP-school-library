a5e7b3c4694a5c54f56568409392b073028cfbd8
keza681 <lindakeza19@gmail.com>
2022-06-18 17:43:51 +0200

    Add logic to student file

--- student.rb
+++ student.rb
@@ -1,4 +1,4 @@
-require './per_son'
+require './person'
 
 class Student < person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
@@ -6,7 +6,15 @@ class Student < person
     @classroom = classroom
   end
 
+  attr_reader :classroom
+
   def play_hooky
     "¯\(ツ)/¯"
   end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
 end
+
